1e396e98c520822d1404e26687fdee04afe41df0
VinNikVla <vinnik1905@yandex.ru>
2021-01-26 17:48:02 +0300

    Нужно сделать класс для отображения всего

--- Client/widget.cpp
+++ Client/widget.cpp
@@ -1,15 +1,31 @@
 #include "widget.h"
 #include "ui_widget.h"
-#include <QSlider>
+
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    QPushButton* buttonEnd = new QPushButton(this);
+    QVBoxLayout* Vlayout = new QVBoxLayout(this);
+    QHBoxLayout* temperature = new QHBoxLayout(this);
+    QLabel* name = new QLabel("Temperature", this);
+    QLCDNumber* num = new QLCDNumber(3, this);
+    QLabel* type = new QLabel("K", this);
+
+    temperature->addWidget(name);
+    temperature->addWidget(num);
+    temperature->addWidget(type);
+
+    //temperature->se
+    Vlayout->addWidget(buttonEnd);
+    Vlayout->addItem(temperature);
+    elementsOnGUI.push_back(buttonEnd);
 
 
 
+    //ui
 }
 
 Widget::~Widget()
--- Client/widget.h
+++ Client/widget.h
@@ -1,7 +1,13 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
+#include <QVector>
 #include <QWidget>
+#include <QSlider>
+#include <QPushButton>
+#include <QLayout>
+#include <QLabel>
+#include <QLCDNumber>
 
 namespace Ui {
 class Widget;
@@ -17,6 +23,9 @@ public:
 
 private:
     Ui::Widget *ui;
+    QVector<QWidget*> elementsOnGUI;
+    //QHBoxLayout* createElement(QWidget*...);
+
 };
 
 #endif // WIDGET_H
